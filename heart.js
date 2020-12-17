26ac7dc60810518a2c2695c5ecb087ae11009787
manthanjgodhasara <69115559+manthanjgodhasara@users.noreply.github.com>
2020-12-17 10:38:34 +0530

    Add files via upload

---
+++ heart.js
@@ -0,0 +1,15 @@
+let heart = document.querySelectorAll(".heart");
+
+heart.forEach(function(button){
+    button.addEventListener("click", function(){
+    if(button.classList.contains("liked")){
+        button.classList.remove("liked");   
+    }
+    else{
+        button.classList.add("liked");
+    }
+    });
+});
+
+
+
